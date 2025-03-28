708b7e45acd2e0896a2098749611eee7510f2e7f
Habiib Dimassi <habib.dimassi03@gmail.com>
2025-03-28 17:25:49 +0100

    resolved bugs

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import CustomUser, Client , AdminUser ,ServiceOwner ,ServicePicture
+from .models import CustomUser, Client , AdminUser ,ServiceOwner ,ServicePicture , PasswordResetToken
 
 admin.site.register(CustomUser)
 admin.site.register(Client)
 admin.site.register(AdminUser)
 admin.site.register(ServiceOwner)
 admin.site.register(ServicePicture)
+admin.site.register(PasswordResetToken)
 
